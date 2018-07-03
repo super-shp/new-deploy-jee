30ea3816ecb47e27e8b5d42448e3ac762bdcb929
caoyong <caoyong@bytedance.com>
2018-07-03 16:40:54 +0800

    init
    Change-Id: Id7cc0dc71085914121bc885a6886c73752d4e4f5

---
+++ src/main/java/com/superxhp/newsdeploy/NewsDeployApplication.java
@@ -0,0 +1,12 @@
+package com.superxhp.newsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsDeployApplication.class, args);
+	}
+}
