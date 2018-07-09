2f98287278765d7d6dd68821ad140380af3fa5db
tfhnia <wjchzndd@163.com>
2018-07-09 10:34:22 +0800

    f

--- SpringNews/src/main/java/com/springnews/controller/StartController.java
+++ SpringNews/src/main/java/com/springnews/controller/StartController.java
@@ -4,6 +4,7 @@ import com.springnews.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.springnews.entity.UserDao;
 
 import java.util.Date;
 import java.util.List;
@@ -29,6 +30,11 @@ public class StartController {
 //        newsContentRepository.save(n);
         //return userRepository.findAll();
         return newsContentRepository.findAll();
+    }
+
+    @GetMapping(value = "/users")
+    public List<User> queryUser(){
+        return userRepository.findAll();
     }
 
 }
--- SpringNews/src/main/java/com/springnews/entity/UserDao.java
+++ SpringNews/src/main/java/com/springnews/entity/UserDao.java
@@ -1,15 +1,35 @@
 package com.springnews.entity;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class UserDao {
+public class UserDao extends JdbcDaoSupport {
     @Autowired
     UserRepository userRepository;
 
     public List<User> userList(){
         return userRepository.findAll();
     }
+
+    public List<User> QueryAllUser() {
+
+        String sql = "select uid,author,username from user";
+        List<Map<String,Object>> list = getJdbcTemplate().queryForList(sql);
+        List<User> userList=new ArrayList<User>();
+        for(Map<String,Object> row:list)
+        {
+            User user=new User();
+            user.setUid((Integer)row.get("uid"));
+            user.setAuthor((String)row.get("author"));
+            user.setUsername((String)row.get("username"));
+            userList.add(user);
+        }
+        return userList;
+
+    }
+
+
 }
