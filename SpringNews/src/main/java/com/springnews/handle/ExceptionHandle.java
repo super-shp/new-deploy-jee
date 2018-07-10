2ab7959e251e60fd5a1bfcdfecf312a11ef5acd7
tfhnia <wjchzndd@163.com>
2018-07-10 23:07:37 +0800

    fi

--- SpringNews/src/main/java/com/springnews/handle/ExceptionHandle.java
+++ SpringNews/src/main/java/com/springnews/handle/ExceptionHandle.java
@@ -1,7 +1,10 @@
 package com.springnews.handle;
 
+import com.springnews.exception.UserException;
 import com.springnews.utils.ResultUtil;
 import com.springnews.utils.UnifyResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,10 +12,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 public class ExceptionHandle {
 
+    private final static Logger logger = LoggerFactory.getLogger(ExceptionHandle.class);
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public UnifyResponse handle(Exception e){
-        return ResultUtil.error(100, e.getMessage());
+        if (e instanceof UserException) {
+            UserException userException = (UserException) e;
+            return ResultUtil.error(userException.getCode(), userException.getMessage());
+        } else {
+            logger.error("[ 系统异常 ] {} ", e);
+            return ResultUtil.error(100, e.getMessage());
+        }
     }
 
 }
