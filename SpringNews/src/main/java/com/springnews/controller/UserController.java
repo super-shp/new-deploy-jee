3bfcf2dfa80909845d7042eb1b464f203238f5e8
tfhnia <wjchzndd@163.com>
2018-07-17 20:52:07 +0800

    修复注册登陆以及查找用户逻辑

--- SpringNews/src/main/java/com/springnews/controller/UserController.java
+++ SpringNews/src/main/java/com/springnews/controller/UserController.java
@@ -1,23 +1,30 @@
 package com.springnews.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.springnews.entity.MyUser;
-import com.springnews.entity.UserRepository;
+import com.google.gson.JsonObject;
+import com.springnews.entity.*;
 import com.springnews.enums.ResultEnum;
 import com.springnews.utils.ResultUtil;
+import com.springnews.utils.UUIDUtils;
 import com.springnews.utils.UnifyResponse;
 import io.jsonwebtoken.Jwts;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/auth")
 public class UserController {
 
+    @Autowired
     private UserRepository applicationUserRepository;
+    @Autowired
+    private UserInfoRepository userInfoRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public UserController(UserRepository myUserRepository,
@@ -33,9 +40,24 @@ public class UserController {
 
     @PostMapping("/signup")
     public UnifyResponse<MyUser> signUp(@RequestBody MyUser user) {
+        // 写入User表
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        System.out.println(user.getUsername());
+        String uid = UUIDUtils.getUUID();
+        user.setUid(uid);
+
+        Timestamp time = new Timestamp(System.currentTimeMillis());
+        user.setCreated_time(time);
+        user.setUpdated_time(time);
+
         applicationUserRepository.save(user);
+
+        // 写入UserInfo表
+        UserInfo userinfo = new UserInfo();
+        userinfo.setLiked(0);
+        userinfo.setUid(uid);
+        userinfo.setFigure("");
+        userinfo.setWords(0);
+        userInfoRepository.save(userinfo);
         return ResultUtil.successs(ResultEnum.OK, user);
     }
 
@@ -43,7 +65,21 @@ public class UserController {
     public UnifyResponse<MyUser> getInfo(@RequestHeader("Authorization") String token) {
         String username = getUserName(token);
         MyUser user = applicationUserRepository.findByUsername(username);
-        return ResultUtil.successs(ResultEnum.OK, user);
+        String uid = user.getUid();
+        UserInfo userInfo = userInfoRepository.findByUid(uid);
+
+        TimeFormatTransform transformer = new TimeFormatTransform();
+        JSONObject infoJson = new JSONObject();
+        infoJson.put("id", user.getId());
+        infoJson.put("uid", user.getUid());
+        infoJson.put("author", user.getAuthor());
+        infoJson.put("likes", userInfo.getLiked());
+        infoJson.put("words", userInfo.getWords());
+        infoJson.put("status", user.getStatus());
+        infoJson.put("created_time", transformer.dateToTimeStamp(user.getCreated_time()));
+        infoJson.put("updated_time", transformer.dateToTimeStamp(user.getUpdated_time()));
+        System.out.println(infoJson);
+        return ResultUtil.successs(ResultEnum.OK, infoJson);
     }
 
     @PostMapping("/resetpassword")
--- SpringNews/src/main/java/com/springnews/entity/UserInfoRepository.java
+++ SpringNews/src/main/java/com/springnews/entity/UserInfoRepository.java
@@ -3,5 +3,5 @@ package com.springnews.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
-
+    public UserInfo findByUid(String uid);
 }
